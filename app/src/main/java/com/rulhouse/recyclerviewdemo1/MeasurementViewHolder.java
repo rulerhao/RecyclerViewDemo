9cd14ae734cab616c2384bfb0e566d7135bf7d45
rulerhao <32173057+rulerhao@users.noreply.github.com>
2022-06-07 14:00:14 +0800

    1. Added delete button in item.

--- app/src/main/java/com/rulhouse/recyclerviewdemo1/MeasurementViewHolder.java
+++ app/src/main/java/com/rulhouse/recyclerviewdemo1/MeasurementViewHolder.java
@@ -1,24 +1,42 @@
 package com.rulhouse.recyclerviewdemo1;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class MeasurementViewHolder extends RecyclerView.ViewHolder {
+    private ViewHolderListener myListener;
+    interface ViewHolderListener {
+        void onDelete(int index);
+    }
+    void setMyListener(ViewHolderListener myListener) {
+        this.myListener = myListener;
+    }
+
     private TextView tempTextView;
     private TextView timeTextView;
+    private Button deleteButton;
 
-    public MeasurementViewHolder(@NonNull View itemView) {
+    public MeasurementViewHolder(@NonNull View itemView, ViewHolderListener listener) {
         super(itemView);
+
+        myListener = listener;
 
         tempTextView = itemView.findViewById(R.id.temp);
         timeTextView = itemView.findViewById(R.id.time);
+        deleteButton = itemView.findViewById(R.id.delete_button);
+        deleteButton.setOnClickListener(deleteButtonClickListener);
     }
 
-    void setMeasurement(Measurement measurement) {
+    void setMeasurement(Measurement measurement, int index) {
         tempTextView.setText(measurement.getTemp().toString());
         timeTextView.setText(measurement.getTimeStamp().toString());
     }
+
+    View.OnClickListener deleteButtonClickListener = v -> {
+        myListener.onDelete(getAdapterPosition());
+    };
 }
--- app/src/main/java/com/rulhouse/recyclerviewdemo1/MeasurementsAdapter.java
+++ app/src/main/java/com/rulhouse/recyclerviewdemo1/MeasurementsAdapter.java
@@ -1,5 +1,6 @@
 package com.rulhouse.recyclerviewdemo1;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 
@@ -11,23 +12,30 @@ import java.util.ArrayList;
 public class MeasurementsAdapter extends RecyclerView.Adapter<MeasurementViewHolder> {
     ArrayList<Measurement> measurements = new ArrayList<Measurement>();
 
+    MeasurementViewHolder.ViewHolderListener listener = new MeasurementViewHolder.ViewHolderListener() {
+        @Override
+        public void onDelete(int index) {
+            deleteMeasurement(measurements.get(index));
+        }
+    };
+
     @NonNull
     @Override
     public MeasurementViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
         return new MeasurementViewHolder(
-                LayoutInflater.from(
-                        parent.getContext()
-                ).inflate(
+                LayoutInflater.from(parent.getContext()).inflate(
                         R.layout.measurement_item,
                         parent,
                         false
-                )
+                ),
+                listener
         );
     }
 
     @Override
     public void onBindViewHolder(@NonNull MeasurementViewHolder holder, int position) {
-        holder.setMeasurement(measurements.get(position));
+        holder.setMeasurement(measurements.get(position), position);
     }
 
     @Override
@@ -41,7 +49,12 @@ public class MeasurementsAdapter extends RecyclerView.Adapter<MeasurementViewHol
 
     public void addMeasurement(Measurement measurement) {
         this.measurements.add(measurement);
-//        notifyItemChanged(getItemCount() - 1);
-        notifyDataSetChanged();
+        notifyItemChanged(getItemCount() - 1);
+    }
+
+    public void deleteMeasurement(Measurement measurement) {
+        int index = measurements.indexOf(measurement);
+        this.measurements.remove(measurement);
+        notifyItemRemoved(index);
     }
 }
